b3a64ef26ca1a0a8da6bdaa1e32c377bd543288f
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-01-28 21:16:46 +0200

    Added mockup for personal info

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 
 import './styles/App.css';
+import  PersonalInfo  from './classes/PersonalInfo';
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return (
-        <>
+    const personalInfo = new PersonalInfo();
 
-        </>
-    );
+    personalInfo.updateEmail('.test@test.com');
+
+    console.log(personalInfo.getEmail());
+
+    return <></>;
 }
 
 export default App;
