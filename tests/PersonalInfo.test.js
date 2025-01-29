3406d79ae23fae6f942994f9cba55746673ee903
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-01-29 21:21:13 +0200

    Removed test for default values and refactored email validation test

--- tests/PersonalInfo.test.js
+++ tests/PersonalInfo.test.js
@@ -6,16 +6,6 @@ test('should initialize PersonalInfo class', () => {
     expect(personalInfo).toBeInstanceOf(PersonalInfo);
 });
 
-test('should return default values in PersonalInfo', () => {
-    const personalInfo = new PersonalInfo();
-
-    expect(personalInfo.fullName).toBe('John Doe');
-    expect(personalInfo.email).toBe('johndoe@mail.com');
-    expect(personalInfo.phone).toBe('1234567');
-    expect(personalInfo.address).toBe('Random City, Random Country');
-    expect(personalInfo.profileSummary).toBe('Lorem ipsum');
-});
-
 test('should update full name', () => {
     const personalInfo = new PersonalInfo();
 
@@ -34,8 +24,8 @@ test('should update email', () => {
     expect(personalInfo.email).toBe('some@some.com');
 });
 
-test('should return error if email test fails regex', () => {
+test('should throw error if email fails regex', () => {
     const personalInfo = new PersonalInfo();
-    const wrongEmail = personalInfo.updateEmail('some!some.com');
-    expect(wrongEmail).toThrow('invalid email');
+
+    expect(() => {personalInfo.updateEmail('some!some.com')}).toThrow(new Error('invalid email'));
 });
