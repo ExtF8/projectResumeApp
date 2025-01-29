0f8b60d862406f4754e9b2b53f7c2317b5ae8e6a
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-01-29 07:58:18 +0200

    Added tests

--- tests/PersonalInfo.test.js
+++ tests/PersonalInfo.test.js
@@ -1,9 +1,41 @@
 import PersonalInfo from '../src/classes/PersonalInfo';
 
+test('should initialize PersonalInfo class', () => {
+    const personalInfo = new PersonalInfo();
+
+    expect(personalInfo).toBeInstanceOf(PersonalInfo);
+});
+
+test('should return default values in PersonalInfo', () => {
+    const personalInfo = new PersonalInfo();
+
+    expect(personalInfo.fullName).toBe('John Doe');
+    expect(personalInfo.email).toBe('johndoe@mail.com');
+    expect(personalInfo.phone).toBe('1234567');
+    expect(personalInfo.address).toBe('Random City, Random Country');
+    expect(personalInfo.profileSummary).toBe('Lorem ipsum');
+});
+
 test('should update full name', () => {
     const personalInfo = new PersonalInfo();
-    const fullName = 'Test User';
-    personalInfo.updateFullName(fullName);
+
+    const firstName = 'Test';
+    const lastName = 'User';
+
+    personalInfo.updateFullName(firstName, lastName);
 
     expect(personalInfo.fullName).toBe('Test User');
+});
+
+test('should update email', () => {
+    const personalInfo = new PersonalInfo();
+    personalInfo.updateEmail('some@some.com');
+
+    expect(personalInfo.email).toBe('some@some.com');
+});
+
+test('should return error if email test fails regex', () => {
+    const personalInfo = new PersonalInfo();
+    const wrongEmail = personalInfo.updateEmail('some!some.com');
+    expect(wrongEmail).toThrow('invalid email');
 });
