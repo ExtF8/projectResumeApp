1bcdc8d9939b8a907122bd7adcb82819d732b310
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-01-31 19:33:17 +0200

    Added Education class

---
+++ src/classes/Education.js
@@ -0,0 +1,9 @@
+export default class Education {
+    constructor(degree, institution, startDate, endDate, description) {
+        this.degree = degree;
+        this.institution = institution;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+}
