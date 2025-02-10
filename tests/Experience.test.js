317e45b7e2582bc6f1677bf29c5570957f8b3d56
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-02-10 09:05:15 +0200

    Added tests for managing experience entries

--- tests/Experience.test.js
+++ tests/Experience.test.js
@@ -38,4 +38,45 @@ describe('Experience Class', () => {
             );
         });
     });
+
+    describe('Managing Experience Entries', () => {
+        test('should add multiple Experience entries', () => {
+            const entryOne = {
+                role: 'Employee',
+                company: 'Employer',
+                startDate: '2010',
+                endDate: '2020',
+                description: 'Did some important work',
+            };
+            const entryTwo = {
+                role: 'Worker',
+                company: 'Some Employer',
+                startDate: '2020',
+                endDate: '2025',
+                description: 'Did some not so important work',
+            };
+
+            experience.addExperienceEntry(entryOne);
+            experience.addExperienceEntry(entryTwo);
+
+            expect(experience.getExperienceEntries()).toEqual([entryOne, entryTwo]);
+            expect(experience.getExperienceEntries().length).toBe(2);
+        });
+
+        test('should update an existing experience entry', () => {
+            const entry = {
+                role: 'Worker',
+                company: 'Some Employer',
+                startDate: '2020',
+                endDate: '2025',
+                description: 'Did some not so important work',
+            };
+
+            experience.addExperienceEntry(entry);
+            const update = experience.updateExperienceEntry(0, { role: 'Manager' });
+
+            expect(update).toBe(true);
+            expect(experience.getExperienceEntries()[0].role)
+        });
+    });
 });
