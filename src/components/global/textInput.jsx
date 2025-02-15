add99c56686a43ccab65cbddd937ee0d0e05a215
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-02-15 13:44:41 +0200

    Added text input component and corresponding style

---
+++ src/components/global/textInput.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+import styles from '../../styles/textInput.module.css';
+
+const TextInput = ({ className, label, errorText, id, ...rest }) => {
+    const [validationMessage, setValidationMessage] = useState('');
+
+    const onInvalid = e => {
+        const target = e.target;
+        setValidationMessage(target.validationMessage);
+    };
+
+    const onBlur = e => {
+        const target = e.target;
+
+        if (!!validationMessage) {
+            setValidationMessage(target.validationMessage);
+        }
+    };
+
+    const wrapperClassName = classNames(className, styles.wrapper);
+
+    return (
+        <div className={wrapperClassName}>
+            <div>
+                {label && (
+                    <div className={styles.label}>
+                        <label htmlFor={id}>{label}</label>
+                    </div>
+                )}
+            </div>
+            <div className='relative'>
+                <input
+                    id={id}
+                    className={styles.input}
+                    onInvalid={onInvalid}
+                    onBlur={onBlur}
+                    {...rest}
+                />
+            </div>
+
+            {!!validationMessage && (
+                <div className={styles.validationMessage}>{errorText || validationMessage}</div>
+            )}
+        </div>
+    );
+};
+
+TextInput.displayName = 'TextInput';
+
+export default TextInput;
